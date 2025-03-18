1ed40eec5e2ca967ac217c777e1226efd2c11f01
Tirumala <84436332+20A91A04M5@users.noreply.github.com>
2025-03-18 12:17:18 +0530

    Update Cardio.jsx

--- src/Nfxcomponents/CardioAndHiit/Cardio.jsx
+++ src/Nfxcomponents/CardioAndHiit/Cardio.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Stopwatches from "../Stopwatch/Stop";
 import { useNavigate } from "react-router-dom";
+import Loading from '../../Loading/Loading';
 
 const buttonStyle = {
   background: "blue",
@@ -43,7 +44,7 @@ function CardioHiitExercises() {
       });
   }, []);
 
-  if (loading) return <div style={{ color: "orange", textAlign:"center", fontSize:"50px", alignContent:"center", height:"100vh",backgroundColor:"black" }}>Loading...</div>;
+  if (loading) return <Loading />;
 
   return (
     <div style={{ padding: "20px", background: "#000", color: "#fff" }}>
