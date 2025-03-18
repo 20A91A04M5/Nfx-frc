432e0c5ed5ecf12faa32b99963cce427bf57ae32
Tirumala <84436332+20A91A04M5@users.noreply.github.com>
2025-03-18 12:13:04 +0530

    Add files via upload

---
+++ src/Loading/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="dumbbell">
+        🏋️‍♂️
+      </div>
+      <p>Loading your workout...</p>
+    </div>
+  );
+};
+
+export default Loading;
