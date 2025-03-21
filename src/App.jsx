8d791b1c4abd285acabf5b33c5ec166192e0338d
Tirumala <84436332+20A91A04M5@users.noreply.github.com>
2025-03-21 21:54:03 +0530

    Update App.jsx
    routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 import "./Land.css";
-// import { logo,im1,im2,im3,im4,im5,im6,im7,im8,im9,im10,im11,im12 } from "./assets";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // If you need the Bootstrap JS components:
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import LoginForm from "./Components/Login";
-// import SignupPage from "./Components/Signup";
+
 import Landingpage from "./Landingpage";
 import { Route, Routes } from "react-router-dom";
 import LoginForm from "./Components/Login";
@@ -23,8 +21,9 @@ import BackAndBiceps from "./Nfxcomponents/BackAndBiceps/BackAndBiceps";
 import Legs from "./Nfxcomponents/Legs/Legs";
 import Shoulders from "./Nfxcomponents/Shoulders/Shoulders";
 import Cardio from "./Nfxcomponents/CardioAndHiit/Cardio";
-// import SaturdayActiveRecoveryExercises from "./Nfxcomponents/Flexible/FLexibility";
 import RestDaySunday from "./Nfxcomponents/RestDay/Rest";
+import SaturdayExercises from "./Nfxcomponents/Flexible/FLexibility";
+
 import Monday from "./Nfxcomponents/Dietplan/MonDiet";
 import Tuesday from "./Nfxcomponents/Dietplan/TuesDiet";
 import Wednesday from "./Nfxcomponents/Dietplan/WedDiet";
@@ -32,16 +31,14 @@ import Thursday from "./Nfxcomponents/Dietplan/ThurDiet";
 import Friday from "./Nfxcomponents/Dietplan/FriDiet";
 import Saturday from "./Nfxcomponents/Dietplan/SatDiet";
 import Sunday from "./Nfxcomponents/Dietplan/SunDiet";
-// import SaturdayActiveRecoveryExercises from "./Nfxcomponents/Flexible/FLexibility";
-// import SaturdayFullBodyExercises from "./Nfxcomponents/Flexible/FLexibility";
-import SaturdayExercises from "./Nfxcomponents/RestDay/Rest";
+
 
 
 
 const App = () => {
   return (
     <>
-      {/* <Landingpage /> */}
+      
       <Routes>
         <Route path="/" element={<Landingpage />}/>
         <Route path="/login" element={<LoginForm />} />
@@ -57,6 +54,7 @@ const App = () => {
         <Route path="/exercises/Flexible" element={<SaturdayExercises />} />
         <Route path="/exercises/Cardio" element={<Cardio />} />
         <Route path="/exercises/Rest" element={<RestDaySunday />} />
+
         <Route path="/diet" element={<DietPlanSection />} />
         <Route path="/diet/monday" element={<Monday />} />
         <Route path="/diet/tuesday" element={<Tuesday />} />
@@ -67,7 +65,6 @@ const App = () => {
         <Route path="/diet/sunday" element={<Sunday />} />
         
         <Route path="/prices" element={<Pricing />} />
-        {/* <Route path="/Schedule" element={<GymSched />} /> */}
       </Routes>
     </>
   );
